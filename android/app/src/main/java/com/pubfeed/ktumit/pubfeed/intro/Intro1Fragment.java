a359e23c9562c117cccff1746da0dcce99e013ea
Ümit Dincel <dincelumit@gmail.com>
2016-05-11 23:14:05 +0300

    initial commit for android

---
+++ android/app/src/main/java/com/pubfeed/ktumit/pubfeed/intro/Intro1Fragment.java
@@ -0,0 +1,22 @@
+package com.pubfeed.ktumit.pubfeed.intro;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pubfeed.ktumit.pubfeed.R;
+
+
+public class Intro1Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_intro1, container, false);
+    }
+
+}
---
+++ android/app/src/main/java/com/pubfeed/ktumit/pubfeed/intro/Intro2Fragment.java
@@ -0,0 +1,20 @@
+package com.pubfeed.ktumit.pubfeed.intro;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pubfeed.ktumit.pubfeed.R;
+
+
+public class Intro2Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_intro2, container, false);
+    }
+
+}
